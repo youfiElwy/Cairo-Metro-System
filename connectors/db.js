1d9fefb09f71b68b5895d59be952980cecd45cbf
AhmedHosny2 <98207790+AhmedHosny2@users.noreply.github.com>
2023-05-21 19:53:43 +0300

    Create connectors folder
    This folder will contain the following
    db.js → our connection
    scripts.sql → DBscripts
    seed.sqk → DBinsertions

---
+++ connectors/db.js
@@ -0,0 +1,12 @@
+const config = {
+	client: 'pg',
+	connection: {
+		host: 'localhost',
+		port: 5432,
+		user: 'postgres',
+		password: '1q2w3e4r',
+		database: 'postgres',
+	},
+};
+
+module.exports = require('knex')(config);
