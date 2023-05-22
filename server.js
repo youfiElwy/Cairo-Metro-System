d28e32f77d199af5c445aee72678c92fdb6a0288
Youssef Elwy <102544122+youfiElwy@users.noreply.github.com>
2023-05-22 21:17:41 +0300

    updated session files stuff

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
+// IMPORTS
 require('dotenv').config();
-
 const express = require('express');
 const app = express();
-
 const knex = require('knex');
 const db = require('./connectors/db');
-
 const bodyParser = require('body-parser');
+
+// IMPORT AUTHENTICATION MIDDLEWARE
+const authMiddleware = require('./middleware/auth');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// MY APIs
+// ALL ROUTES
 const signup_route = require('./routes/public/user_sign_up');
 const login_route = require('./routes/public/user_login');
 const refund_request_route = require('./routes/private/user_tickets/user_refund_request');
@@ -24,28 +26,36 @@ const pay_for_subscription_route = require('./routes/private/user_subscriptions/
 const pay_for_ticket_route = require('./routes/private/user_tickets/pay_for_ticket');
 const rideStarted = require('./routes/private/user_rides/simulate_ride_start');
 const rideEnded = require('./routes/private/user_rides/simulate_ride_end');
+const auth = require('./middleware/auth');
 
-// ALL ROUTES
+// PUBLIC ROUTES
 signup_route(app);
 login_route(app);
-refund_request_route(app);
-get_users_route(app);
-delete_users_route(app);
 user_forgot_password_route(app);
 user_forgot_password_verify_route(app);
 user_forgot_password_new_password_route(app);
+get_users_route(app);
+delete_users_route(app);
+
+// CALL AUTHENTICATION MIDDLEWARE
+app.use(authMiddleware);
+
+// PRIVATE ROUTES
+refund_request_route(app);
 get_user_subscription_route(app);
 pay_for_subscription_route(app);
 pay_for_ticket_route(app);
 rideStarted(app);
 rideEnded(app);
 
+// HANDLE IF WE DID NOT FIND THE ROUTE WE WERE LOOKING FOR
 app.use(function (req, res, next) {
 	return res
 		.status(404)
 		.send('----------  ERROR COULD NOT FIND YOUR SPECIFIED ROUTE/PAGE  -----------');
 });
 
+// START SERVER
 app.listen(3000, () => {
 	console.log('Server is now listening at port 3000 on http://localhost:3000/');
 });
