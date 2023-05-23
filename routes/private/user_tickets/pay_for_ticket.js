7a20a01b4ced9e4c8caa95b569ef7e97cbd28cab
Youssef Elwy <102544122+youfiElwy@users.noreply.github.com>
2023-05-23 17:44:43 +0300

    Update pay_for_ticket.js

--- routes/private/user_tickets/pay_for_ticket.js
+++ routes/private/user_tickets/pay_for_ticket.js
@@ -181,4 +181,134 @@ module.exports = function (app) {
 			}
 		}
 	});
+
+	// -------------------------------------------------------------------------------------------------------
+	// -------------------------------------------------------------------------------------------------------
+	// -------------------------------------------------------------------------------------------------------
+	// -------------------------------------------------------------------------------------------------------
+	// -------------------------------------------------------------------------------------------------------
+
+	// app.post('/api/v1/payment/ticket/', async function (req, res) {
+	// 	const { origin, destination } = req.body;
+
+	// 	const user = await getUser(req);
+	// 	const userId = user.user_id;
+
+	// 	// Step 1: Check if the operation is valid
+
+	// 	// Check subscription status
+	// 	db.query('SELECT status FROM subscriptions WHERE user_id = $1', [userId], (err, result) => {
+	// 		if (err) {
+	// 			console.error('Error executing query', err);
+	// 			return res.status(500).json({ error: 'Internal server error' });
+	// 		}
+
+	// 		const subscriptionStatus = result.rows[0]?.status;
+	// 		if (subscriptionStatus !== 'active') {
+	// 			return res.status(400).json({ error: 'Your subscription has expired' });
+	// 		}
+
+	// 		// Check the number of usages
+	// 		db.query(
+	// 			'SELECT numberOfUsages, maxNumberOfUsages FROM subscriptions WHERE user_id = $1',
+	// 			[userId],
+	// 			(err, result) => {
+	// 				if (err) {
+	// 					console.error('Error executing query', err);
+	// 					return res.status(500).json({ error: 'Internal server error' });
+	// 				}
+
+	// 				const { numberOfUsages, maxNumberOfUsages } = result.rows[0];
+
+	// 				if (numberOfUsages >= maxNumberOfUsages) {
+	// 					return res.status(400).json({
+	// 						error: 'You have reached the maximum number of usages for your subscription',
+	// 					});
+	// 				}
+
+	// 				// Check the zone of the target session
+	// 				db.query(
+	// 					`SELECT z.minimumStations, z.maximumStations, pr.number_of_stations
+	// 			 FROM zones z
+	// 			 JOIN possible_routes pr ON pr.origin = $1 AND pr.destination = $2
+	// 			 WHERE z.zone_id = (SELECT zone_id FROM subscriptions WHERE user_id = $3)`,
+	// 					[origin, destination, userId],
+	// 					(err, result) => {
+	// 						if (err) {
+	// 							console.error('Error executing query', err);
+	// 							return res.status(500).json({ error: 'Internal server error' });
+	// 						}
+
+	// 						const { minimumStations, maximumStations, number_of_stations } =
+	// 							result.rows[0];
+	// 						if (maximumStations < minimumStations) {
+	// 							return res.status(400).json({ error: 'Invalid zone configuration' });
+	// 						}
+
+	// 						if (number_of_stations > maximumStations) {
+	// 							return res.status(400).json({
+	// 								error: 'The zone of the target session is not within your subscription',
+	// 							});
+	// 						}
+
+	// 						// Step 2: Proceed with purchasing the ticket and assigning a ride
+
+	// 						// Increase the number of usages and check if it reaches the maximum
+	// 						const newNumberOfUsages = numberOfUsages - 1;
+	// 						const ticketStatus = (newNumberOfUsages = maxNumberOfUsages
+	// 							? 'expired'
+	// 							: 'active');
+
+	// 						// Update the subscription with the new number of usages and status
+	// 						db.query(
+	// 							'UPDATE subscriptions SET numberOfUsages = $1, status = $2 WHERE user_id = $3',
+	// 							[newNumberOfUsages, ticketStatus, userId],
+	// 							(err) => {
+	// 								if (err) {
+	// 									console.error('Error executing query', err);
+	// 									return res.status(500).json({ error: 'Internal server error' });
+	// 								}
+
+	// 								// Create a ticket
+	// 								db.query(
+	// 									`INSERT INTO ticket (trans_ID, status, user_ID, possible_routes_id, zone_id)
+	// 						VALUES ($1, $2, $3, (SELECT possible_routes_id FROM possible_routes WHERE origin = $4 AND destination = $5), (SELECT zone_id FROM subscriptions WHERE user_id = $6))
+	// 						RETURNING ticket_ID`,
+	// 									[transactionId, ticketStatus, userId, origin, destination, userId],
+	// 									(err, result) => {
+	// 										if (err) {
+	// 											console.error('Error executing query', err);
+	// 											return res.status(500).json({ error: 'Internal server error' });
+	// 										}
+
+	// 										const ticketId = result.rows[0]?.ticket_ID;
+
+	// 										// Create a ride
+	// 										db.query(
+	// 											'INSERT INTO ride (status, start_time, end_time, ticket_ID) VALUES ($1, $2, $3, $4)',
+	// 											['upcoming', null, null, ticketId],
+	// 											(err) => {
+	// 												if (err) {
+	// 													console.error('Error executing query', err);
+	// 													return res
+	// 														.status(500)
+	// 														.json({ error: 'Internal server error' });
+	// 												}
+
+	// 												// Return the successful response
+	// 												return res.json({
+	// 													message: 'Ticket purchased successfully',
+	// 												});
+	// 											}
+	// 										);
+	// 									}
+	// 								);
+	// 							}
+	// 						);
+	// 					}
+	// 				);
+	// 			}
+	// 		);
+	// 	});
+	// });
 };
