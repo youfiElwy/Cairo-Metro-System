7816da60d1b23d1008de3492bfbe158d9af76a54
Ahmed Yehia <ahmed.hosny4434@gmail.com>
2023-05-26 18:23:05 +0300

    Update db.js

--- connectors/db.js
+++ connectors/db.js
@@ -4,9 +4,21 @@ const config = {
 		host: 'localhost',
 		port: 5432,
 		user: 'postgres',
-		password: 'Stecki10',
-		database: 'Metro_System_Semester_4_Projekt',
+		password: '1234',
+		database: 'Cairo_Metro_DB',
 	},
 };
 
 module.exports = require('knex')(config);
+
+// // yehia DB
+// const config = {
+// 	client: 'pg',
+// 	connection: {
+// 		host: 'localhost',
+// 		port: 5432,
+// 		user: 'postgres',
+// 		password: '1234',
+// 		database: 'Cairo_Metro_DB',
+// 	},
+// };
