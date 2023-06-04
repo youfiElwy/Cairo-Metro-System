d327dc377457fa5cdfe5e9a785093c728e83f9d8
Youssef Elwy <102544122+youfiElwy@users.noreply.github.com>
2023-06-04 12:01:34 +0300

    Update seed.sql

--- connectors/seed.sql
+++ connectors/seed.sql
@@ -33,16 +33,16 @@ ALTER SEQUENCE sessions_sessionid_seq RESTART WITH 1;
 
 
 
-Insert Into users Values(DEFAULT, 'username1', 'email1', 'passwort1', '12/31/1999', 27, 'M', 'phone1', 'ssn1', 'normal', 'user');
-Insert Into users Values(DEFAULT, 'username2', 'email2', 'passwort2', '12/31/1999', 30, 'F', 'phone2', 'ssn2', 'normal', 'user');
-Insert Into users Values(DEFAULT, 'username3', 'email3', 'passwort3', '12/31/1999', 87, 'F', 'phone3', 'ssn3', 'normal', 'user');
-Insert Into users Values(DEFAULT, 'username4', 'email4', 'passwort4', '12/31/1999', 28, 'M', 'phone4', 'ssn4', 'normal', 'user');
-Insert Into users Values(DEFAULT, 'username5', 'email5', 'passwort5', '12/31/1999', 27, 'M', 'phone5', 'ssn5', 'normal', 'user');
+Insert Into users Values(DEFAULT, 'username1', 'user1', '893e3c3e629a5e5cdd70fb69333ecba742128d9e2861eae5c12a57e1aa1d5b05a375dde9a69fd7045700ecca5495c05e9767d26574c8fe3f84077203f2898d34','4a49a720f0fd7f1dd18b80fa8e77724b', '12/31/1999', 27, 'M', 'phone1', 'ssn1', 'normal', 'user');
+Insert Into users Values(DEFAULT, 'username2', 'user2', 'ac820ab401a308f973a4ae7d2498f2f81ed2b10c8cf156ad5f8e5f3631b32a5bcb34c8f869de5ce55c4c48f295e926e9df9d14c877a7d066186b30b7294aadb1','9076eb3f5fac109a69eae9fd191fb417', '12/31/1999', 30, 'F', 'phone2', 'ssn2', 'normal', 'user');
+Insert Into users Values(DEFAULT, 'username3', 'user3', '60cd9e4906c2e8bad574eb692ebf1badb18c6e4a46a3f7a7833e7a05b208335f54bd1d9803d8be6f824c695df8ce91538e7effd39d2485dd628d9cae34d9910b','7d1756b8adbb36ec1d59e7f78ad3681b', '12/31/1999', 87, 'F', 'phone3', 'ssn3', 'normal', 'user');
+Insert Into users Values(DEFAULT, 'username4', 'user4', 'b0f62a4273d25716813af4132cb659173bf2c53164d1b5c7e4c8cffc21745e0a874fd9af30196010c1f779f4e824e13d8145cfd18c2f2649a54694d82f3c254d','e4a4c55c0fa939235d34122295bf170a','12/31/1999', 28, 'M', 'phone4', 'ssn4', 'normal', 'user');
+Insert Into users Values(DEFAULT, 'username5', 'user5', 'b1436ae5b660286d92f386f2f28b19f79ea558683d8120520bba918b2e649f217d54ea579f01d3732b8736a5f40808485d39412e460bd8edbd994889d08d1deb','154d3384b510a3e7a4e3e49554eca2ae', '12/31/1999', 27, 'M', 'phone5', 'ssn5', 'normal', 'user');
 
-Insert Into users Values(DEFAULT, 'Joe',    'email6', 'passwort6', '12/31/1999', 20, 'M', 'phone6', 'ssn6', 'normal', 'superadmin');
-Insert Into users Values(DEFAULT, 'Bahy',   'email7', 'passwort7', '12/31/1999', 20, 'M', 'phone7', 'ssn7', 'normal', 'superadmin');
-Insert Into users Values(DEFAULT, 'Ahmed',  'email8', 'passwort8', '12/31/1999', 20, 'M', 'phone8', 'ssn8', 'normal', 'superadmin');
-Insert Into users Values(DEFAULT, 'Ossama', 'email9', 'passwort9', '12/31/1999', 20, 'M', 'phone9', 'ssn9', 'normal', 'superadmin');
+Insert Into users Values(DEFAULT, 'Joe',    'Joe', '784a8a9f0845600d49f9e10c922e1d8cec1fd487c20dc75ef2e589efbe08b16677abfb822e7ed14db5ecc98358578f03d2ef02fbae095c25f7c6ad595bdbf3c4','718086d6ecf60858a10ac9d9159fa045','12/31/1999', 20, 'M', 'phone6', 'ssn6', 'normal', 'superadmin');
+Insert Into users Values(DEFAULT, 'Bahy',   'Bahy', 'd278de30babd793d96569f83754b478946c6611e7a79c8517ccb1f68e7e195fd7b9435362f4676e6f10b27629bf6aa8a7a447b8ebeeb265fab58a3301974a2d6','dafde46963be8d282ccdbf1c0a6f0500','12/31/1999', 20, 'M', 'phone7', 'ssn7', 'normal', 'superadmin');
+Insert Into users Values(DEFAULT, 'Ahmed',  'Ahmed', 'bc664da92fee855d538756aa3ded5fc805ca2f5823d8fc36f649d31d6a8476c0f0d3456e3eb5f3a75e452454f65b442fe1720bc7cf9ada455ca94f1338ac8195','1d002039f2d4e9a6bd55fc0333b03a66','12/31/1999', 20, 'M', 'phone8', 'ssn8', 'normal', 'superadmin');
+Insert Into users Values(DEFAULT, 'Ossama', 'Ossama', '845c69bb69bc7534197a659bc1715ccbad6f386c5a45de93075320238c8e150d453628a4a24c4d841ed8a46725c9070edd44fde3e75c87c5f67215fe3465b5cd','8cb15a7ae8cc5973eb436647227b8394','12/31/1999', 20, 'M', 'phone9', 'ssn9', 'normal', 'superadmin');
 
 
 
