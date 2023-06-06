68073648e83482a5071867519d01cd2cea6d2e1f
Youssef Elwy <102544122+youfiElwy@users.noreply.github.com>
2023-06-06 23:12:00 +0300

    updated forgot password

--- routes/public/forgot_pass/user_forgot_password_new_password.js
+++ routes/public/forgot_pass/user_forgot_password_new_password.js
@@ -6,34 +6,39 @@ module.exports = function (app) {
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({ extended: true }));
 
-	app.put('/api/v1/users/forgot_password/new_password/:token', async function (req, res) {
+	app.put('/api/v1/users/forgot_password/new_password/', async function (req, res) {
 		try {
-			const { password } = req.body;
-			const { token } = req.params;
+			const { password, resetToken } = req.body;
+			// const { token } = req.params;
+			console.log(password);
+			console.log(resetToken);
 
-			const tokenStillExists = await db('users').select('*').where('reset_token', token);
+			// const tokenStillExists = await db('users').select('*').where('reset_token', resetToken);
+			const tokenStillExists = await db('users').select('*').where('reset_token', resetToken);
 
 			if (isEmpty(tokenStillExists)) {
-				return res.status(400).send('Token invalid/ expired!');
+				return res.status(400).json([400, 'Token invalid/ expired!']);
 			}
 
 			const updatedUser = await db('users')
-				.where('reset_token', token)
+				.where('reset_token', resetToken)
 				.update({
 					password,
 					reset_token: null,
 					reset_token_expiration: null,
 				})
 				.returning('*');
+
 			if (isEmpty(updatedUser)) {
 				return res
-					.status(400)
-					.send('Could not find user in the database to update their password!');
+					.status(401)
+					.json([401, 'Could not find user in the database to update their password!']);
 			}
-			return res.status(200).json(updatedUser);
+			console.log('1');
+			return res.status(200).send([200, 'Successfully reset password']);
 		} catch (err) {
 			console.log('error message', err.message);
-			return res.status(400).send('Could not create new user password');
+			return res.status(403).json([403, 'Could not create new user password']);
 		}
 	});
 };
