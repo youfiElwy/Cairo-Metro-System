37b11cba38a63375ef539d93193b8163a2d70deb
bahylol <bahymohamed2010@gmail.com>
2023-06-07 13:37:05 +0300

    added stripe for user subscription

---
+++ routes/private/stripe/cancel_stripe.js
@@ -0,0 +1,31 @@
+const { isEmpty } = require('lodash');
+const db = require('../../../connectors/db');
+const bodyParser = require('body-parser');
+const getUser = require('../../public/get_user');
+
+module.exports = function (app) {
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.post('/stripe/cancel', async function (req, res) {
+        const user = await getUser(req);
+        const { user_id } = user;
+        const { payment_token } = req.body;
+        try {
+            const updateToken = await db("users")
+                .where("user_id", user_id)
+                .andWhere("payment_token", payment_token)
+                .update({
+                    payment_token_active: false
+                })
+                .returning("*");
+            updateToken;
+            return res.status(200).send([200, 'payment canceled']);
+        }
+        catch (err) {
+            return res
+                .status(400)
+                .send([400, 'payment canceled']);
+        }
+    })
+}
--- routes/private/stripe/subscription_stripe.js
+++ routes/private/stripe/subscription_stripe.js
@@ -3,6 +3,7 @@ const { isEmpty } = require('lodash');
 const db = require('../../../connectors/db');
 const bodyParser = require('body-parser');
 const getUser = require('../../public/get_user');
+const crypto = require('crypto');
 const stripe = require("stripe")("sk_test_51NFilGFJ1nJmZISKGcm36DxSEkCfPHgGbP44wZnP9hNTQzeHPoSOu7kOpagXhnquESo1SNrbIJI96deIxYyz2mUO00dj0rZfpY")
 
 module.exports = function (app) {
@@ -11,13 +12,13 @@ module.exports = function (app) {
 
   app.post("/create-checkout-session-subscription", async (req, res) => {
     const user = await getUser(req);
-    const userId = user.user_id;
+    const { user_id } = user;
 
     // check if user is already subscribed
     const subExists = await db
       .select('*')
       .from('subscriptions')
-      .where('user_id', userId)
+      .where('user_id', user_id)
       .andWhere('status', 'active');
     if (!isEmpty(subExists)) {
       return res.status(400).send([400, 'user is already subscribed to an active plan']);
@@ -27,28 +28,44 @@ module.exports = function (app) {
     let sub_numberofusages = 0;
     let name = "";
     let description = "";
-    let zone_id=req.body.zone_id;
-    let duration=req.body.duration;
-    if (duration=== 'monthly') {
+    const { zone_id } = req.body;
+    const { duration } = req.body;
+    const payment_token = crypto.randomBytes(16).toString('hex');
+
+    const payment = await db("users")
+      .where("user_id", user_id)
+      .update({
+        payment_token: payment_token,
+        payment_token_active: true
+      })
+      .returning("*");
+    payment;
+    if (duration === 'monthly') {
       trans_amount = 100;
       sub_maxnumberofusages = 15;
       sub_numberofusages = 15;
       name = "Monthly Subscription";
       description = "Max number of usages : 15"
     } else if (duration === 'quarterly') {
-      trans_amount = 150;
+      trans_amount = 200;
       sub_maxnumberofusages = 150;
       sub_numberofusages = 150;
       name = "Quarterly Subscription";
       description = "Max number of usages : 150"
     } else {
-      trans_amount = 250;
+      trans_amount = 400;
       sub_maxnumberofusages = 400;
       sub_numberofusages = 400;
       name = "Yearly Subscription";
       description = "Max number of usages : 400"
     }
-    const test = [{
+    if (zone_id === "2") {
+      trans_amount = trans_amount * 1.2;
+    }
+    else if (zone_id === "3") {
+      trans_amount = trans_amount * 1.5;
+    }
+    const item = [{
       price_data: {
         currency: "egp",
         product_data: {
@@ -59,18 +76,18 @@ module.exports = function (app) {
       },
       quantity: 1
     }];
-    console.log(test);
+    console.log(item);
     try {
       const session = await stripe.checkout.sessions.create({
         payment_method_types: ["card"],
         mode: "payment",
-        line_items: test,
-        success_url: `http://localhost:5000/test?duration=${duration}&zone_id=${zone_id}`,
-        cancel_url: `http://localhost:5000/${trans_amount}`,
+        line_items: item,
+        success_url: `http://localhost:5000/SubscriptionStripe?status=accepted&duration=${duration}&zone_id=${zone_id}&payment_token=${payment_token}`,
+        cancel_url: `http://localhost:5000/SubscriptionStripe?status=rejected&payment_token=${payment_token}`,
       })
-      res.json(session.url )
+      res.json([200, session.url])
     } catch (e) {
-      res.status(500).json({ error: e.message })
+      res.status(500).json([500, e.message])
     }
   })
 
