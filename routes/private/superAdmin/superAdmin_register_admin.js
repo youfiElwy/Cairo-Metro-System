85973d8da906deb51c56bfd8f4465da869a789b8
Youssef Elwy <102544122+youfiElwy@users.noreply.github.com>
2023-06-10 05:50:32 +0300

    dred

--- routes/private/superAdmin/superAdmin_register_admin.js
+++ routes/private/superAdmin/superAdmin_register_admin.js
@@ -2,16 +2,24 @@ const { isEmpty } = require('lodash');
 const db = require('../../../connectors/db');
 const bodyParser = require('body-parser');
 const getUser = require('../../../routes/public/get_user');
+const crypto = require('crypto');
+
+function hashPassword(password) {
+	const salt = crypto.randomBytes(16).toString('hex');
+	const hash = crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex');
+	return [salt, hash];
+}
 
 module.exports = function (app) {
-	console.log('WESELTTT');
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({ extended: true }));
+
 	app.post('/api/v1/superadmin/registeradmin', async function (req, res) {
+		console.log('HENAAA');
 		console.log(req.body);
 		const userExists = await db.select('*').from('users').where('email', req.body.email);
 		if (!isEmpty(userExists)) {
-			return res.status(401).send('user exists');
+			return res.status(401).send([401, 'user exists']);
 		}
 		const currentDate = new Date();
 		const birthdate = new Date(req.body.birthdate);
@@ -38,7 +46,7 @@ module.exports = function (app) {
 			const email = req.body.email;
 			const password = req.body.password;
 
-			return res.status(200);
+			return res.status(200).send([200, 'successfully created']);
 		} catch (err) {
 			console.log(err.message);
 			return res.status(400).send([400, 'Error: Could not register user']);
