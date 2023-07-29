92f267bf84fdbffe046b7a0a65b594e6fafabb20
Youssef Elwy <102544122+youfiElwy@users.noreply.github.com>
2023-07-29 18:14:19 +0300

    updated db connection

--- connectors/db.js
+++ connectors/db.js
@@ -1,11 +1,31 @@
+// const config = {
+// 	client: 'pg',
+// 	connection: {
+// 		host: 'localhost',
+// 		port: 5432,
+// 		user: 'postgres',
+// 		password: 'Stecki10',
+// 		database: 'Metro_System_Semester_4_Projekt',
+// 	},
+// };
+
+// module.exports = require('knex')(config);
+// console.log('HEREE');
+// console.log(process.env.POSTGRES_HOST);
+// console.log(process.env.PORT);
+// console.log(process.env.POSTGRES_USER);
+// console.log(process.env.POSTGRES_PASSWORD);
+// console.log(process.env.POSTGRES_DATABASE);
+
 const config = {
 	client: 'pg',
 	connection: {
-		host: 'localhost',
+		host: process.env.POSTGRES_HOST,
 		port: 5432,
-		user: 'postgres',
-		password: 'Stecki10',
-		database: 'Metro_System_Semester_4_Projekt',
+		user: process.env.POSTGRES_USER,
+		password: process.env.POSTGRES_PASSWORD.toString(),
+		database: process.env.POSTGRES_DATABASE,
+		ssl: true,
 	},
 };
 
